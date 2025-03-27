0ad12f72fdd4bc0e7cdf6abebdcaf72425770242
Rishi Mishra <156692992+rishi-2811@users.noreply.github.com>
2025-03-27 16:26:08 +0530

    Time: 11 ms (73.22%), Space: 48.1 MB (65.5%) - LeetHub

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int smallestDivisor(int[] piles, int h) {
+        int max=Integer.MIN_VALUE;
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<piles.length;i++){
+            max=Math.max(max,piles[i]);
+        }
+        int low=1;
+        int high=max;
+        int mid;
+        while(low<=high){
+            mid=(low+high)/2;
+            long k=eatingBanana(piles,mid);
+            if(k<=h){
+                high=mid-1;
+                ans=Math.min(ans,mid);
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public long eatingBanana(int[] piles, int k){
+        long sum=0;
+        long a=0;
+        for(int i=0;i<piles.length;i++){
+            if(piles[i]%k==0){
+                a=piles[i]/k;
+            }
+            else{
+                a=piles[i]/k+1;
+            }
+            sum+=a;
+        }
+        
+        return sum;
+    }
+}
