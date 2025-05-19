2d710bc32e10263c2264a854fd0f29c7a7be5193
Rishi Mishra <156692992+rishi-2811@users.noreply.github.com>
2025-05-19 12:21:29 +0530

    Time: 0 ms (100%), Space: 43.4 MB (5.34%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -3,30 +3,30 @@ class Solution {
         List<Integer> s=new ArrayList<>();
         List<List<Integer>> ans=new ArrayList<>();
         List<List<Integer>> answer=new ArrayList<>();
-        getAnswer(ans,s,nums.length);
-        for(List<Integer> a:ans){
-            List<Integer> b=new ArrayList<>();
-            for(int i=0;i<nums.length;i++){
-                int c=a.get(i);
-                if(c==1){
-                    b.add(nums[i]);
-                }
-            }
-            answer.add(b);
-        }
+        getAnswer(answer,s,nums,0);
+        // for(List<Integer> a:ans){
+        //     List<Integer> b=new ArrayList<>();
+        //     for(int i=0;i<nums.length;i++){
+        //         int c=a.get(i);
+        //         if(c==1){
+        //             b.add(nums[i]);
+        //         }
+        //     }
+        //     answer.add(b);
+        
         return answer;
     }
-    public static void getAnswer(List<List<Integer>> answer,List<Integer> s,int n){
-        if(s.size()==n){
+    public static void getAnswer(List<List<Integer>> answer,List<Integer> s,int[] nums,int i){
+        if(i==nums.length){
             answer.add(s);
         }
         else{
             List<Integer> a=new ArrayList<>(s);
-            a.add(0);
-            getAnswer(answer,a,n);
+            
+            getAnswer(answer,a,nums,i+1);
             List<Integer> b=new ArrayList<>(s);
-            b.add(1);
-            getAnswer(answer,b,n);
+            b.add(nums[i]);
+            getAnswer(answer,b,nums,i+1);
             
         }
     }
