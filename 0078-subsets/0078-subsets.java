cffadaadaabcf212035ff626d26e862f85d6e32f
Rishi Mishra <156692992+rishi-2811@users.noreply.github.com>
2025-05-19 13:01:37 +0530

    Time: 0 ms (100%), Space: 43 MB (30.13%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,8 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<Integer> s=new ArrayList<>();
-        List<List<Integer>> ans=new ArrayList<>();
         List<List<Integer>> answer=new ArrayList<>();
         getAnswer(answer,s,nums,0);
-        // for(List<Integer> a:ans){
-        //     List<Integer> b=new ArrayList<>();
-        //     for(int i=0;i<nums.length;i++){
-        //         int c=a.get(i);
-        //         if(c==1){
-        //             b.add(nums[i]);
-        //         }
-        //     }
-        //     answer.add(b);
-        
         return answer;
     }
     public static void getAnswer(List<List<Integer>> answer,List<Integer> s,int[] nums,int i){
